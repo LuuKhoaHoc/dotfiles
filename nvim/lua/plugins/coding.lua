653a3975a4874c28fbc344401025aaf6e58e6c2d
khoahoc <lkhoc200443@gmail.com>
2025-11-28 15:34:38 +0700

    feat: Add `fast-cursor-move.nvim` and `vscode-multi-cursor.nvim` plugins, and configure `LuaSnip` with history and delete check events.

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -55,25 +55,29 @@ return {
     dependencies = {
       "saghen/blink.compat",
       -- optional: provides snippets for the snippet source
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
-          return
-        end
-        return "make install_jsregexp"
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        {
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
+            return
+          end
+          return "make install_jsregexp"
+        end)(),
+        dependencies = {
           "rafamadriz/friendly-snippets",
-          config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-            require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath "config" .. "/snippets" } }
-          end,
+        },
+        config = function(_, opts)
+          if opts then require("luasnip").setup(opts) end
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath "config" .. "/snippets" } }
+        end,
+        opts = {
+          history = true,
+          delete_check_events = "TextChanged",
         },
       },
     },
